e6d90f7d057b1dbc6d4b90589945a7b1d1dbf3c3
Probe-Konto <entwickl.konto@gmail.com>
2025-06-30 19:13:17 +0300

    second try

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Multistep-Form/",
+  base: "/multistep-form/",
   plugins: [react(), tailwindcss()],
 });
